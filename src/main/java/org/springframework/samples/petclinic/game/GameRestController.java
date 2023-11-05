7c39397f34bfc43fd607e23518ba3672a3dc593a
Fernando José De Celis Hurtado <ferdehur@alum.us.es>
2023-11-05 12:40:38 +0100

    Clase game corregida

--- src/main/java/org/springframework/samples/petclinic/game/GameRestController.java
+++ src/main/java/org/springframework/samples/petclinic/game/GameRestController.java
@@ -31,7 +31,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/game")
-@Tag(name = "Games", description = "API for the  management of  Games.")
+@Tag(name = "Games", description = "API for the  management of games.")
 @SecurityRequirement(name = "bearerAuth")
 public class GameRestController {
     GameService gs;
