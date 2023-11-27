48ebe860168521d126b267f28578d632a9f6528a
Gonzalo Navas Remmers <gonnavrem@alum.us.es>
2023-11-27 20:32:52 +0100

    Cambio en el funcionamiento de las rutas del lobby

--- frontend/src/lobby/index.js
+++ frontend/src/lobby/index.js
@@ -15,29 +15,26 @@ import { MdAdd } from "react-icons/md";
 
 export default function Lobby() {
     const [roles, setRoles] = useState([]);
-    const jwt = tokenService.getLocalAccessToken();
-    const [collapsed, setCollapsed] = useState(true);
-    const [player, setPlayer] = useState({})
-    const myUsername = jwt_decode(jwt).sub;
+    const [myPlayer, setMyPlayer] = useState({})
     const [game, setGame] = useState({});
     const [numPlayers, setNumPlayers] = useState(2);
-    const [visible, setVisible] = useState(true);
-    const [players, setPlayers] = useState([])
+    const [players, setPlayers] = useState([]);
+    const jwt = tokenService.getLocalAccessToken();
+    const myUsername = jwt_decode(jwt).sub;
+
+    const gameId = window.location.href.split("/")[4] // extrae la id de la partida desde la ruta spliteandola por las / en un array, cuidado que el indice del array que devuelve el split no empieza en [0] sino en [1] por algu motivo
 
     useEffect(() => {
         if (jwt) {
             setRoles(jwt_decode(jwt).authorities);
-            GetCurrentPlayer(myUsername);
-            GetCurrentPlayer("player2");
-            GetCurrentPlayer("player3");
-            GetCurrentPlayer("player4");
-            GetCurrentPlayer("player5");
+            GetCurrentPlayer();
+            GetCurrentGame();
 
         }
     }, [jwt])
 
-    function GetCurrentPlayer(username) {
-        fetch("/api/v1/players?username=" + username, {
+    function GetCurrentPlayer() {
+        fetch("/api/v1/players?username=" + myUsername, {
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${jwt}`,
@@ -45,34 +42,29 @@ export default function Lobby() {
             method: "GET"
         })
             .then(response => response.json())
-            .then(response => {
-                username === myUsername ? setPlayer(response[0]) : console.log("no es mi player");
-                players.push(response[0])
-            })
-            .then(setPlayers(players))
+            .then(response => { setMyPlayer(response[0]) })
     }
 
-    function CreateGame() {
-        const newGame = {
-            numPlayers: numPlayers,
-            status: "WAITING",
-            players: players,
-        }
-        fetch("/api/v1/games", {
+    async function GetCurrentGame() {
+        const currentGame = await fetchCurrentGame()
+        setGame(currentGame)
+    }
+
+    async function fetchCurrentGame() {
+        const response = await fetch(`/api/v1/games/${gameId}`, {
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${jwt}`,
             },
-            method: "POST",
-            body: JSON.stringify(newGame)
+            method: "GET"
         })
-            .then(response => response.json())
-            .then(response => setGame(response))
+        const fetchedGame = await response.json();
+        return fetchedGame
     }
 
-    const playerList = players.map(player =>
+    const playerList = JSON.stringify(game) === "{}" ? null : game.players.map(player =>   //se está comprobando si game es un objeto vacío para que no de problemas al leer undefined de game.players antes de que el estado adquiera valor
         <li key={player.id}>
-            <div className="list-item-container" style={{marginBottom:"20"}}>
+            <div className="list-item-container" style={{ marginBottom: "20" }}>
                 <img className="profile-picture" src={player.profilePicture} />
                 <div className="list-player-name">
                     {myUsername}
@@ -82,81 +74,9 @@ export default function Lobby() {
 
     return (
         <div className="lobby-page-container">
-            <div>
-                <Modal isOpen={visible} centered="true" className="modal" style={{ height: "65%" }}>
-                    <ModalHeader style={{ color: "white", textShadow: "2px 2px 2px #00000020" }}>
-                        Select the number of players
-                    </ModalHeader>
-                    <ModalBody style={{ flexDirection: "row" }}>
-                        <Button className="modal-button" style={{
-                            backgroundColor: "#ff4a4a",
-                            border: "none",
-                            borderRadius: "50%",
-                            textAlign: "center",
-                            fontSize: 25,
-                            color: "white",
-                            boxShadow: "3px 3px 5px #00000020",
-                            textShadow: "2px 2px 2px #00000020"
-                        }}
-                            onClick={() => numPlayers > 2 ? setNumPlayers(numPlayers - 1) : null}
-                        >
-                            -
-                        </Button>
-                        <p style={{
-                            paddingTop: 15,
-                            marginLeft: 5,
-                            marginRight: 5,
-                            textAlign: "center",
-                            fontSize: 22,
-                            color: "white",
-                            textShadow: "2px 2px 2px #00000020",
-                        }}>
-                            {numPlayers}
-                        </p>
-                        <Button className="modal-button" style={{
-                            transition: "0.15s",
-                            backgroundColor: "#59ff75",
-                            border: "none",
-                            borderRadius: "50%",
-                            textAlign: "center",
-                            fontSize: 25,
-                            boxShadow: "3px 3px 5px #00000020",
-                            textShadow: "2px 2px 2px #00000020",
-                        }}
-                            onClick={() => numPlayers < 5 ? setNumPlayers(numPlayers + 1) : null}
-                        >
-                            +
-                        </Button>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button className="done-button" style={{
-                            backgroundColor: "#ffa952", border: "none", boxShadow: "5px 5px 5px #00000020", textShadow: "2px 2px 2px #00000020", transition: "0.15s",
-                        }} onClick={() => {
-                            setVisible(false);
-                            setPlayers(players.slice(0, numPlayers))  // para probar con varios jugadores antes de implementar uniones al lobby
-                        }}>
-                            Done
-                        </Button>
-                    </ModalFooter>
-                </Modal>
-            </div>
-            {/*
-            <div style={{ marginTop: 70 }}>
-                <svg width="650" height="650">
-                    <rect className="player-list"
-                        x="50"
-                        y="20"
-                        rx="20"
-                        ry="20"
-                        width="400"
-                        height="600"
-                        style={{ fill: "black", opacity: 0.5 }} />
-                </svg>
-            </div>
-*/}
             <div className="hero-div">
-                <h1>Players {players.length}/{numPlayers}</h1>
-                
+                <h1>Players {JSON.stringify(game) === "{}" ? "0" : game.players.length}/{numPlayers}</h1> {/*misma comprobacion que arriba pero esta vez para players.length*/}
+
                 <ul className="ul-players">
                     {playerList}
                 </ul>
@@ -193,8 +113,7 @@ export default function Lobby() {
                     transition: "0.15s",
                     marginBottom: 20
                 }} onClick={() => {
-                    console.log(players)
-                    CreateGame()
+                    console.log(game.players)
                 }}>
                     pruebita xd
                 </Button>
