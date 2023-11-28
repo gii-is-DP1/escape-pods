af975229fb611c63efde7f8dc02c00de535aeb1a
Davgodfer <davgodfer@alum.us.es>
2023-11-28 01:27:48 +0100

    Funcionalidad Join

--- frontend/src/lobby/index.js
+++ frontend/src/lobby/index.js
@@ -111,6 +111,7 @@ export default function Lobby() {
                     marginBottom: 20
                 }} onClick={() => {
                     console.log(game.players)
+                    GetCurrentGame()
                 }}>
                     pruebita xd
                 </Button>
